b1d7861ca6e4721ea3240abe7d3e9381208efc51
mkmdev <mkmdev18@gmail.com>
2020-11-24 09:59:11 +0100

    created dummy data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,61 @@
 <template>
   <div>
     <h1>home</h1>
+    <p>{{ title }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  name: "home",
+
+  comments: {
+    //  Alkomponensek (child), amiket ez a komponens használ
+  },
+
+  data() {
+    // statikus adatok
+    return {
+      title: "Home",
+      tasks: [
+        {
+          id: 1,
+          name: "First test task",
+          completed: false,
+          due: "2020-11-24"
+        },
+        {
+          id: 2,
+          name: "Second test task",
+          completed: true,
+          due: "2020-12-04"
+        },
+        {
+          id: 3,
+          name: "Third test task",
+          completed: false,
+          due: "2020-11-25"
+        },
+        {
+          id: 4,
+          name: "Fourth test task",
+          completed: false,
+          due: "2020-12-31"
+        }
+      ]
+    };
+  },
+
+  computed: {
+    //  dinamikus adatok
+  },
+
+  created() {
+    //  ng oninit()
+  },
+
+  methods: {
+    //  komponens használata közben használt methodok
+  }
+};
+</script>
